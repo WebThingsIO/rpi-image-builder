38a00acc577fb564afd497c0615544e4bc1a9faf
Dave Hylands <dhylands@gmail.com>
2018-07-19 13:47:53 -0700

    Change language to minimal (#3)
    * Change language to minimal
    * Just build openzwave and gateway
    * Updated trigger.sh to support prefix and removed base and image
    
    This requires add-gateway.sh to be run locally.

--- config.sh
+++ config.sh
@@ -18,12 +18,8 @@ setvar RPXC               './bin/rpxc'
 # Currently we're not caching enough stuff for the docker image to work properly, so leave
 # USE_CACHED_RPXC set to 0 until we figure out what all needs to be cached.
 setvar USE_CACHED_RPXC    0
-setvar USE_CACHED_BASE    1
-setvar GATEWAY_REPO       https://github.com/mozilla-iot/gateway
-setvar GATEWAY_BRANCH     v0.4.0
+setvar GATEWAY_REPO       https://github.com/dhylands/gateway
+setvar GATEWAY_BRANCH     remove-base-after-unzip
 setvar OPENZWAVE_ZIP      https://codeload.github.com/OpenZWave/open-zwave/zip/ab5fe966fee882bb9e8d78a91db892a60a1863d9
-setvar BASE_IMAGE_DIR     https://s3-us-west-1.amazonaws.com/mozillagatewayimages/base
-setvar BASE_IMAGE_NAME    gateway-0.4.0-base-desktop.img
-# The defaut image name is the base image name minus the -base portion
-setvar GATEWAY_IMAGE_NAME "${BASE_IMAGE_NAME/-base/}"
+setvar TAR_PREFIX         0.5.0-pre1
 echo "==================================================================================="
