0ee137324b7922e3da5f938490d251f7547303fd
James Hobin <hobinjk@gmail.com>
2018-07-26 16:17:01 -0400

    Point default values to mozilla-iot

--- config.sh
+++ config.sh
@@ -18,8 +18,8 @@ setvar RPXC               './bin/rpxc'
 # Currently we're not caching enough stuff for the docker image to work properly, so leave
 # USE_CACHED_RPXC set to 0 until we figure out what all needs to be cached.
 setvar USE_CACHED_RPXC    0
-setvar GATEWAY_REPO       https://github.com/dhylands/gateway
-setvar GATEWAY_BRANCH     remove-base-after-unzip
+setvar GATEWAY_REPO       https://github.com/mozilla-iot/gateway
+setvar GATEWAY_BRANCH     master
 setvar OPENZWAVE_ZIP      https://codeload.github.com/OpenZWave/open-zwave/zip/ab5fe966fee882bb9e8d78a91db892a60a1863d9
 setvar TAR_PREFIX         0.5.0-pre1
 echo "==================================================================================="
